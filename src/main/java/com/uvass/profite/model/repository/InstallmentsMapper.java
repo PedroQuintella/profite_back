daf07e9838b24e7260d30f3b543848ba5b7b6a1c
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2021-11-20 18:42:27 -0300

    Feat: Criação dos Mappers
    Criados os Mappers do sistema.

---
+++ src/main/java/com/uvass/profite/model/repository/InstallmentsMapper.java
@@ -0,0 +1,18 @@
+package com.uvass.profite.model.repository;
+
+import com.uvass.profite.model.entity.Installments;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class InstallmentsMapper implements RowMapper<Installments> {
+
+    @Override
+    public Installments mapRow(ResultSet result, int i) throws SQLException {
+        Installments installments = new Installments();
+        installments.setId(result.getInt("id"));
+        installments.setCount(result.getInt("count"));
+        installments.setValue(result.getFloat("value"));
+        return installments;
+    }
+}
---
+++ src/main/java/com/uvass/profite/model/repository/ProductMapper.java
@@ -0,0 +1,22 @@
+package com.uvass.profite.model.repository;
+
+import com.uvass.profite.model.entity.Product;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductMapper implements RowMapper<Product> {
+
+    @Override
+    public Product mapRow(ResultSet result, int i) throws SQLException {
+        Product product = new Product();
+        product.setId(result.getInt("id"));
+        product.setName(result.getString("name"));
+        product.setImage(result.getString("image"));
+        product.setOldPrice(result.getFloat("oldPrice"));
+        product.setPrice(result.getFloat("price"));
+        product.setDescription(result.getString("description"));
+        product.setInstallments_id(result.getInt("installments_id"));
+        return product;
+    }
+}
