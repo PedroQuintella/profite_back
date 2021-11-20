f76b24d6ea442c8d0feb9739f64c2ae6408ded81
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2021-11-20 17:36:36 -0300

    Feat: Criação das Entidades
    Criadas as entidades do sistema.

---
+++ src/main/java/com/uvass/profite/model/entity/Product.java
@@ -0,0 +1,76 @@
+package com.uvass.profite.model.entity;
+
+public class Product {
+    private int id;
+    private String name;
+    private String image;
+    private float oldPrice;
+    private float price;
+    private String description;
+    private Installments installments;
+    private int installments_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public float getOldPrice() {
+        return oldPrice;
+    }
+
+    public void setOldPrice(float oldPrice) {
+        this.oldPrice = oldPrice;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Installments getInstallments() {
+        return installments;
+    }
+
+    public void setInstallments(Installments installments) {
+        this.installments = installments;
+    }
+
+    public int getInstallments_id() {
+        return installments_id;
+    }
+
+    public void setInstallments_id(int installments_id) {
+        this.installments_id = installments_id;
+    }
+}
