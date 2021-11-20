498559ae17decfac5235ac1365ef5cfaa1aa1908
Pedro Quintella <pedro.q2000@outlook.com>
2021-11-20 04:30:09 -0300

    Feat: Configurações iniciais
    Configurações iniciais do Banco de Dados.

---
+++ src/main/java/com/uvass/profite/ProfiteBackApplication.java
@@ -0,0 +1,13 @@
+package com.uvass.profite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfiteBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfiteBackApplication.class, args);
+    }
+
+}
